6f0be979e9ecac6774981609236ab45ee6a659f7
Sujal Pahwa <142010621+sujalpahwa30@users.noreply.github.com>
2024-08-01 02:50:34 +0530

    Added the code for basic implementation of React and Vite and understanding its flow and structure

---
+++ basicsofvite/src/App.jsx
@@ -0,0 +1,13 @@
+import YouTube from './Youtube';
+
+function App() {
+  return (
+    <>
+   <h1>A classic vite app</h1>
+   <h1>Vite react app</h1>
+   <YouTube />
+   </>
+  )
+}
+
+export default App;
---
+++ basicsofvite/src/Youtube.jsx
@@ -0,0 +1,7 @@
+function YouTube() {
+    return (
+        <h1>YouTube App</h1>
+    )
+}
+
+export default YouTube;
