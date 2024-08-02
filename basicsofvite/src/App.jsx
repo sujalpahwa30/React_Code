8e7c060a9fa7b4698d424bce788513c9a26a517e
Sujal Pahwa <142010621+sujalpahwa30@users.noreply.github.com>
2024-08-03 02:59:23 +0530

    Created my own react library and JSX

--- basicsofvite/src/App.jsx
+++ basicsofvite/src/App.jsx
@@ -1,10 +1,11 @@
 import YouTube from './Youtube';
 
 function App() {
+  const userName = 'Sujal Pahwa'
   return (
     <>
-   <h1>A classic vite app</h1>
-   <h1>Vite react app</h1>
+   <h1>A classic vite app {2+2}</h1>
+   <h1>Vite react app{userName} </h1>
    <YouTube />
    </>
   )
--- basicsofvite/src/main.jsx
+++ basicsofvite/src/main.jsx
@@ -1,11 +1,40 @@
+/* eslint-disable no-unused-vars */
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx';
 
+const reactElement ={
+    type: 'a',
+    props: {
+        href: 'https://www.google.com',
+        target: '_blank',
+    },
+    childen: 'Click me to visit Google'
+}
 
+function myApp() {
+    return (
+        <div>
+            <h1>Custom React App</h1>
+        </div>
+    )
+}
+const AnotherElement = (
+    <a href="https://google.com" target = '_blank'>Visit Google</a>
+)
+
+const areactElement = React.createElement(
+    'a',
+    {
+        href: 'https://www.google.com',
+        target: '_blank'
+    },
+    'Click me to visit Google'
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
-    <App />
+
+    areactElement 
+    // <App/>
   
 )
