5ed214a2ebd205f0759ead69b8821dff8c088ca5
Sujal Pahwa <142010621+sujalpahwa30@users.noreply.github.com>
2024-08-13 03:53:29 +0530

    Adding the code for Context API in React

---
+++ 08miniContext/src/components/Profile.jsx
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import UserContext from "../context/UserContext";
+
+function Profile() {
+    const { user } = useContext(UserContext)
+
+    if (!user) {
+        return <h1>Not logged in </h1>
+    }
+    
+    return (
+        <div>
+            <h1> Profile : {user.username} </h1>
+        </div>
+    )
+}
+
+export default Profile;
